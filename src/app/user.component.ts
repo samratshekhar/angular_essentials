04d0fc7fbf7ecb617b55c1738bd7efe00621e956
Samrat <samratshekhar42@gmail.com>
2018-07-28 21:32:02 +0530

    test string interpolation

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
-    <p>Hello</p>
+    <p>Hello {{name}}</p>
     <p>I'm the user component</p>
   `
 })
-export class UserComponent {}
+export class UserComponent {
+  name = 'Samrat';
+}
